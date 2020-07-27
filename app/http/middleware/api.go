3cf5e8d32202ef74e42bdca2ab6e3539bce0839c
Reindert Vetter <reindert@myparcel.nl>
2020-07-27 23:07:57 +0200

    lanvard/lanvard#66 json body decode

--- app/http/middleware/api.go
+++ app/http/middleware/api.go
@@ -8,5 +8,5 @@ import (
 // This standard api middleware must be
 // run before you can make adjustments to the request
 var Api = []inter.HttpMiddleware{
-	foundation.RequestJsonBody{},
+	foundation.RequestBodyDecoder{},
 }
