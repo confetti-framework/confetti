f0649b764899dac52fa1a5ea669f5e18c75e584d
Reindert Vetter <reindert@myparcel.nl>
2020-08-04 22:53:49 +0200

    lanvard/lanvard#66 get raw from value object

--- app/http/kernel.go
+++ app/http/kernel.go
@@ -3,11 +3,13 @@ package http
 import (
 	"github.com/lanvard/contract/inter"
 	"github.com/lanvard/foundation/http"
+	foundation "github.com/lanvard/foundation/http/middleware"
 	"lanvard/app/http/middleware"
 )
 
 var middlewares = []inter.HttpMiddleware {
 	// todo remove or use ValidatePostSize
+	foundation.RequestBodyDecoder{},
 	middleware.ValidatePostSize{},
 	middleware.RouteModelBinding{},
 }
--- app/http/middleware/api.go
+++ app/http/middleware/api.go
@@ -2,11 +2,8 @@ package middleware
 
 import (
 	"github.com/lanvard/contract/inter"
-	foundation "github.com/lanvard/foundation/http/middleware"
 )
 
-// This standard api middleware must be
-// run before you can make adjustments to the request
 var Api = []inter.HttpMiddleware{
-	foundation.RequestBodyDecoder{},
+	//
 }
