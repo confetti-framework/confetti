fb0ddcaa8a1fb37443fa3c59b9040e3a882c0adb
Reindert Vetter <reindert@myparcel.nl>
2020-09-15 23:18:57 +0200

    lanvard/lanvard#56 gest parameters from request

--- routes/api.go
+++ routes/api.go
@@ -14,7 +14,8 @@ import (
 | Here is where you can register API routes for your application. By default
 | this is loaded in a group. The group is assigned to the "Api" middleware
 | and is placed with "/api" prefix. Feel free to remove the prefix if you
-| are using a subdomain for your API. Enjoy building your API!
+| are using a subdomain for your API (which is recommended).
+| Enjoy building your API!
 |
 */
 
