def3f915095fdbad35e20e97af848d6bb947d9ad
Reindert Vetter <reindert@myparcel.nl>
2020-10-04 22:55:28 +0200

    lanvard/lanvard#68 log error to file

--- config/logging.go
+++ config/logging.go
@@ -29,12 +29,10 @@ var Logging = struct {
 	   |--------------------------------------------------------------------------
 	   |
 	   | Here you may configure the log channels for your application. Out of
-	   | the box, Laravel uses the Monolog PHP logging library. This gives
+	   | the box, Laravel uses the sirupsen/logrus logging library. This gives
 	   | you a variety of powerful log handlers / formatters to utilize.
 	   |
-	   | Available Drivers: "single", "daily", "slack", "syslog",
-	   |                    "errorlog",
-	   |                    "custom", "stack"
+	   | Available Drivers: "single", "daily", "slack", "stack", "custom"
 	   |
 	*/
 	Channels: map[string]inter.LogChannel{
