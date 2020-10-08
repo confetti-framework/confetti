87f586153e7230eae44d75750e504822a79b1b32
Reindert Vetter <reindert@myparcel.nl>
2020-10-08 22:24:55 +0200

    lanvard/lanvard#68 init stack loggers

--- config/logging.go
+++ config/logging.go
@@ -2,60 +2,59 @@ package config
 
 import (
 	"github.com/lanvard/contract/inter"
-	"github.com/lanvard/foundation/drivers"
-	"github.com/lanvard/support/env"
-	"github.com/sirupsen/logrus"
+	"github.com/lanvard/foundation/loggers"
+	"github.com/lanvard/syslog"
 )
 
 var Logging = struct {
-	Default  string
-	Channels map[string]inter.LogChannel
+	Default string
+	Loggers map[string]inter.Logger
 }{
 	/*
 	   |--------------------------------------------------------------------------
-	   | Default Log Channel
+	   | Default Logger
 	   |--------------------------------------------------------------------------
 	   |
-	   | This option defines the default log channel that gets used when writing
+	   | This option defines the default logger that gets used when writing
 	   | messages to the logs. The name specified in this option should match
-	   | one of the channels defined in the "channels" configuration array.
+	   | one of the loggers defined in the "Loggers" configuration.
 	   |
 	*/
 	Default: "stack",
 
 	/*
 	   |--------------------------------------------------------------------------
-	   | Log Channels
+	   | Log Loggers
 	   |--------------------------------------------------------------------------
 	   |
-	   | Here you may configure the log channels for your application. Out of
-	   | the box, Laravel uses the sirupsen/logrus logging library. This gives
+	   | Here you may configure the log loggers for your application. Out of
+	   | the box, Laravel uses the lanvard/logrus logging library. This gives
 	   | you a variety of powerful log handlers / formatters to utilize.
 	   |
 	   | Available Drivers: "single", "daily", "slack", "stack", "custom"
 	   |
 	*/
-	Channels: map[string]inter.LogChannel{
-		"stack": drivers.Stack{
-			Channels: []string{"single"},
+	Loggers: map[string]inter.Logger{
+		"stack": loggers.Stack{
+			Loggers: []string{"single"},
 		},
 
-		"single": drivers.LogRus{
-			Path:  Path.Storage + "/logs/lanvard.log",
-			Level: logrus.DebugLevel,
+		"single": loggers.Syslog{
+			Path:     Path.Storage + "/logs/lanvard.log",
+			MinLevel: syslog.INFO,
 		},
 
-		"daily": drivers.LogRus{
-			Path:  Path.Storage + "/logs/lanvard.log",
-			Level: logrus.DebugLevel,
-			Days:  14,
-		},
-
-		"slack": drivers.Slack{
-			Url:      env.StringOr("LOG_SLACK_WEBHOOK_URL", ""),
-			Username: "Lanvard log",
-			Emoji:    ":boom:",
-			Level:    logrus.ErrorLevel,
-		},
+		// "daily": loggers.Syslog{
+		// 	Path:  Path.Storage + "/logs/lanvard.log",
+		// 	MinLevel: syslog.INFO,
+		// 	Days:  14,
+		// },
+		//
+		// "slack": loggers.Slack{
+		// 	Url:      env.StringOr("LOG_SLACK_WEBHOOK_URL", ""),
+		// 	Username: "Lanvard log",
+		// 	Emoji:    ":boom:",
+		// 	Level:    logrus.ErrorLevel,
+		// },
 	},
 }
