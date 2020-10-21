f1c803898fbadf1a4c9dff01b23b9f1e2ba55db7
Reindert Vetter <reindert@myparcel.nl>
2020-10-22 00:28:03 +0200

    lanvard/lanvard#73 catch panic in latest middleware

--- app/http/middleware/api.go
+++ app/http/middleware/api.go
@@ -2,10 +2,8 @@ package middleware
 
 import (
 	"github.com/lanvard/contract/inter"
-	"github.com/lanvard/foundation/http/middleware"
-	"github.com/lanvard/routing/outcome"
 )
 
 var Api = []inter.HttpMiddleware{
-	middleware.PanicToResponse{Outcome: outcome.Json},
+	//
 }
