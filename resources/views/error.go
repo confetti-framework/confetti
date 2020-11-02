0f6b3183cc6cf45e3e6935437a2d668693d7a0a7
Reindert Vetter <reindert@myparcel.nl>
2020-11-02 23:25:23 +0100

    lanvard/lanvard#76 Define full template path in views

--- resources/views/error.go
+++ resources/views/error.go
@@ -5,6 +5,7 @@ import (
 	"github.com/lanvard/contract/inter"
 	"github.com/lanvard/errors"
 	"github.com/lanvard/support/str"
+	"lanvard/config"
 )
 
 type errorView struct {
@@ -14,27 +15,20 @@ type errorView struct {
 	Status     int
 	AppName    string
 	Locale     string
-	app        inter.App
 	template   string
 }
 
 func Error(app inter.App, err error) inter.View {
-	errorMessage := str.UpperFirst(fmt.Sprintf("%v", err))
 	status, _ := errors.FindStatus(err)
 
 	return &errorView{
-		Message:    errorMessage,
+		Message:    str.UpperFirst(fmt.Sprintf("%v", err)),
 		StackTrace: StackTrace(app, err),
 		Status:     status,
 		AppName:    AppName(app),
 		Locale:     Locale(app),
-		app:        app,
-		template:   "error.gohtml",
+		template:   config.Path.Views + "/error.gohtml",
 	}
-}
-
-func (e errorView) App() inter.App {
-	return e.app
 }
 
 func (e errorView) Template() string {
