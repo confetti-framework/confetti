bcf3b50d8c1f048978631c70862f190ea3a3134d
Reindert Vetter <reindert@myparcel.nl>
2020-11-06 23:57:23 +0100

    lanvard/lanvard#76 explane predefined templates

--- app/providers/view_service_provider.go
+++ app/providers/view_service_provider.go
@@ -10,8 +10,20 @@ type ViewServiceProvider struct{}
 
 // Define your router model bindings, pattern filters, etc.
 func (v ViewServiceProvider) Register(container inter.Container) inter.Container {
-	container.Singleton("template_builder", func(template *template.Template) (*template.Template, error) {
-		return template.ParseGlob(config.Path.Views + "/*")
+	container.Singleton("template_builder", func(templateBuilder *template.Template) (*template.Template, error) {
+		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*/*/*.gohtml"); t != nil {
+			templateBuilder = t
+		}
+		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*/*.gohtml"); t != nil {
+			templateBuilder = t
+		}
+		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*.gohtml"); t != nil {
+			templateBuilder = t
+		}
+		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*.gohtml"); t != nil {
+			templateBuilder = t
+		}
+		return templateBuilder.ParseGlob(config.Path.Views + "/*.gohtml")
 	})
 
 	return container
