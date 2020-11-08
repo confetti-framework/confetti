ece071684b2d16c0fc0cbe0660e3f9a0cb3c05ef
Reindert Vetter <reindert@myparcel.nl>
2020-11-09 00:01:51 +0100

    lanvard/lanvard#76 Add 'Lanvard is under development'

--- app/http/middleware/route_model_binding.go
+++ app/http/middleware/route_model_binding.go
@@ -7,6 +7,8 @@ import (
 type RouteModelBinding struct{}
 
 func (b RouteModelBinding) Handle(request inter.Request, next inter.Next) inter.Response {
+
+	// Feel free to bind your models here.
 	// request.App().Bind("user", func() model.User {
 	// 	return model.User.Find(request.Parameter("user"))
 	// })
