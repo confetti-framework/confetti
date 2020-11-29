25440a011b603e0dffdcaa2c51f628aef9f266fb
Reindert Vetter <reindert@myparcel.nl>
2020-11-29 23:43:04 +0100

    lanvard/lanvard#88 slace track traces on seperated lines

--- app/providers/view_service_provider.go
+++ app/providers/view_service_provider.go
@@ -4,6 +4,7 @@ import (
 	"github.com/lanvard/contract/inter"
 	"html/template"
 	"lanvard/config"
+	"strings"
 )
 
 type ViewServiceProvider struct{}
@@ -11,20 +12,35 @@ type ViewServiceProvider struct{}
 // Define your router model bindings, pattern filters, etc.
 func (v ViewServiceProvider) Register(container inter.Container) inter.Container {
 	container.Singleton("template_builder", func(templateBuilder *template.Template) (*template.Template, error) {
-		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*/*/*.gohtml"); t != nil {
-			templateBuilder = t
-		}
-		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*/*.gohtml"); t != nil {
-			templateBuilder = t
-		}
-		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*.gohtml"); t != nil {
-			templateBuilder = t
-		}
-		if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*.gohtml"); t != nil {
-			templateBuilder = t
-		}
-		return templateBuilder.ParseGlob(config.Path.Views + "/*.gohtml")
+		templateBuilder = addFunctions(templateBuilder)
+
+		return addTemplates(templateBuilder)
 	})
 
 	return container
+}
+
+func addFunctions(templateBuilder *template.Template) *template.Template {
+	return templateBuilder.Funcs(template.FuncMap{
+		"Replace": func(input, from, to string) string {
+			return strings.Replace(input, from, to, -1)
+		},
+		"Trim": strings.Trim,
+	})
+}
+
+func addTemplates(templateBuilder *template.Template) (*template.Template, error) {
+	if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*/*/*.gohtml"); t != nil {
+		templateBuilder = t
+	}
+	if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*/*.gohtml"); t != nil {
+		templateBuilder = t
+	}
+	if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*/*.gohtml"); t != nil {
+		templateBuilder = t
+	}
+	if t, _ := templateBuilder.ParseGlob(config.Path.Views + "/*/*.gohtml"); t != nil {
+		templateBuilder = t
+	}
+	return templateBuilder.ParseGlob(config.Path.Views + "/*.gohtml")
 }
--- resources/views/error.go
+++ resources/views/error.go
@@ -6,28 +6,29 @@ import (
 	"github.com/lanvard/errors"
 	"github.com/lanvard/support/str"
 	"lanvard/config"
+	"strings"
 )
 
 func Error(app inter.App, err error) inter.View {
 	status, _ := errors.FindStatus(err)
 
 	return &ErrorView{
-		Message:    str.UpperFirst(fmt.Sprintf("%v", err)),
-		StackTrace: StackTrace(app, err),
-		Status:     status,
-		AppName:    AppName(app),
-		Locale:     Locale(app),
-		template:   config.Path.Views + "/error.gohtml",
+		Message:     str.UpperFirst(fmt.Sprintf("%v", err)),
+		StackTraces: strings.Split(StackTrace(app, err), "\n"),
+		Status:      status,
+		AppName:     AppName(app),
+		Locale:      Locale(app),
+		template:    config.Path.Views + "/error.gohtml",
 	}
 }
 
 type ErrorView struct {
-	Message    string
-	StackTrace string
-	Status     int
-	AppName    string
-	Locale     string
-	template   string
+	Message     string
+	StackTraces []string
+	Status      int
+	AppName     string
+	Locale      string
+	template    string
 }
 
 func (e ErrorView) Template() string {
