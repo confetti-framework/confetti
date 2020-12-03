fee13ba687b308880f2b423ebddf4e0acc15da40
Reindert Vetter <reindert@myparcel.nl>
2020-12-03 23:42:05 +0100

    lanvard/lanvard#88 get field from error

--- app/http/controllers/user.go
+++ app/http/controllers/user.go
@@ -15,7 +15,7 @@ func UserCreate(request inter.Request) inter.Response {
 func UserStore(request inter.Request) inter.Response {
 	content := request.Content()
 	failures := val.Validate(content,
-		val.Verify("name", rule.Required{}, rule.StringAble{}, rule.Max{Max: 255}),
+		val.Verify("name", rule.Required{}, rule.StringAble{}, rule.MaxCharacters{Max: 255}),
 		val.Verify("email", rule.Required{}, rule.StringAble{}),
 	)
 	if len(failures) > 0 {
