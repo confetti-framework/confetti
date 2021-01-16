b47e3a57d89741272201246655adb404d85c22fa
Reindert Vetter <reindert@myparcel.nl>
2021-01-16 21:37:15 +0100

    confetti-framework/docs#12 Use singleton for fetching user

--- app/http/middleware/route_model_binding.go
+++ app/http/middleware/route_model_binding.go
@@ -9,7 +9,7 @@ type RouteModelBinding struct{}
 func (b RouteModelBinding) Handle(request inter.Request, next inter.Next) inter.Response {
 
 	// Feel free to bind your models here.
-	// request.App().Bind("user", func() model.User {
+	// request.App().Singleton("user", func() model.User {
 	// 	userId := request.Parameter("user_id").Int()
 	// 	return model.FindUser(userId)
 	// })
