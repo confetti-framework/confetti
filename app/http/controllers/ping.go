b6c74dbf48c9fcabf6d097801ee83c23d289f5f7
Reindert Vetter <reindert@myparcel.nl>
2021-01-25 23:42:23 +0100

    93 fix, ping retuns an error.

--- app/http/controllers/ping.go
+++ app/http/controllers/ping.go
@@ -2,11 +2,10 @@ package controllers
 
 import (
 	"github.com/confetti-framework/contract/inter"
-	"github.com/confetti-framework/errors"
 	"github.com/confetti-framework/routing/outcome"
 	net "net/http"
 )
 
 func Ping(_ inter.Request) inter.Response {
-	return outcome.Html(errors.New("pong").Status(net.StatusOK))
+	return outcome.Html("pong").Status(net.StatusOK)
 }
