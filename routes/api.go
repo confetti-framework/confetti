b3dd5c05911269451991c47c72ecbad91799827d
Reindert Vetter <reindert@myparcel.nl>
2021-02-05 23:45:11 +0100

    #93 clean up routers

--- routes/api.go
+++ routes/api.go
@@ -3,22 +3,22 @@ package routes
 import (
 	"confetti-framework/app/http/controllers"
 	"confetti-framework/app/http/middleware"
-	"github.com/confetti-framework/foundation/http/routing"
+	. "github.com/confetti-framework/foundation/http/routing"
 )
 
 /*
-|---------------------------------------------------------------------------
-| API routes
-|---------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. By default
-| this is loaded in a group. The group is assigned to the "Api" middleware
-| and is placed with "/api" prefix. Feel free to remove the prefix if you
-| are using a subdomain for your API (which is recommended).
-| Enjoy building your API!
-|
+	|---------------------------------------------------------------------------
+	| API routes
+	|---------------------------------------------------------------------------
+	|
+	| Here is where you can register API routes for your application. By default
+	| this is loaded in a group. The group is assigned to the "Api" middleware
+	| and is placed with "/api" prefix. Feel free to remove the prefix if you
+	| are using a subdomain for your API (which is recommended).
+	| Enjoy building your API!
+	|
 */
 
-var Api = routing.Group(
-	routing.Get("/ping", controllers.Ping),
+var Api = Group(
+	Get("/ping", controllers.Ping),
 ).Prefix("/api").Middleware(middleware.Api...)
--- routes/web.go
+++ routes/web.go
@@ -3,19 +3,19 @@ package routes
 import (
 	"confetti-framework/app/http/controllers"
 	"confetti-framework/app/http/middleware"
-	"github.com/confetti-framework/foundation/http/routing"
+	. "github.com/confetti-framework/foundation/http/routing"
 )
 
 /*
-|--------------------------------------------------------------------------
-| Web routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register Web routes for your website. By default
-| these routes are loaded within a group which is assigned the "Web"
-| middleware. Enjoy building your website!
-|
+	|--------------------------------------------------------------------------
+	| Web routes
+	|--------------------------------------------------------------------------
+	|
+	| Here is where you can register Web routes for your website. By default
+	| these routes are loaded within a group which is assigned the "Web"
+	| middleware. Enjoy building your website!
+	|
 */
-var Web = routing.Group(
-	routing.Get("/", controllers.Homepage),
+var Web = Group(
+	Get("/", controllers.Homepage),
 ).Middleware(middleware.Web...)
