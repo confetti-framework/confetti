00a59b1a6f236926d0a10c0a5d80d99001292630
Reindert Vetter <reindert@myparcel.nl>
2021-03-04 18:39:50 +0100

    confetti-framework/confetti#105 clean up providers

--- app/http/decorator/register_providers.go
+++ app/http/decorator/register_providers.go
@@ -1,13 +1,12 @@
 package decorator
 
 import (
-	"github.com/confetti-framework/contract/inter"
 	"confetti-framework/app/providers"
+	"github.com/confetti-framework/contract/inter"
 )
 
 type RegisterProviders struct{}
 
-// Providers are located in config/providers/providers.go
 func (r RegisterProviders) Bootstrap(container inter.Container) inter.Container {
 	for _, bootstrapper := range providers.Providers.RegisterProviders {
 		container = bootstrapper.Register(container)
